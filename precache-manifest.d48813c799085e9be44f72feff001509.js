e1f7e13ad635c13546b0067e21c534826b225471
Teemu Koivisto <tkoivisto456@gmail.com>
2019-04-17 23:18:24 +0300

    Deploy first version to github

---
+++ precache-manifest.d48813c799085e9be44f72feff001509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d93782ba921ea6eeba5ed82fe1ff8c0c",
+    "url": "/intro-to-data-visualization-project/static/media/guardian-polls.d93782ba.csv"
+  },
+  {
+    "revision": "b74f7e5b9605c7ed80bd9ec34228269b",
+    "url": "/intro-to-data-visualization-project/static/media/guardian-election-results.b74f7e5b.csv"
+  },
+  {
+    "revision": "96e2d5c086546355f8b4",
+    "url": "/intro-to-data-visualization-project/static/js/runtime~main.04e39f13.js"
+  },
+  {
+    "revision": "bf22377b23a609705fc2",
+    "url": "/intro-to-data-visualization-project/static/js/main.c4a3b5a6.chunk.js"
+  },
+  {
+    "revision": "495fdc70559a0233f853",
+    "url": "/intro-to-data-visualization-project/static/js/2.21b57686.chunk.js"
+  },
+  {
+    "revision": "bf22377b23a609705fc2",
+    "url": "/intro-to-data-visualization-project/static/css/main.26072405.chunk.css"
+  },
+  {
+    "revision": "3331b7ca9543f14c79da6c4233abe9c0",
+    "url": "/intro-to-data-visualization-project/index.html"
+  }
+];
