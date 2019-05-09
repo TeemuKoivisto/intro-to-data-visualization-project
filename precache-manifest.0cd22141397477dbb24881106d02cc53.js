75b2360ac2f73b5a764df83118bf8019c80a77f4
Teemu Koivisto <tkoivisto456@gmail.com>
2019-05-09 09:42:01 +0300

    Version 0.3.0

---
+++ precache-manifest.0cd22141397477dbb24881106d02cc53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6067d4d6229e10abbf8f69a89f7a7e61",
+    "url": "/intro-to-data-visualization-project/static/media/guardian-polls.6067d4d6.csv"
+  },
+  {
+    "revision": "08c2060f28278aad67bd6bbb93d701dd",
+    "url": "/intro-to-data-visualization-project/static/media/guardian-election-results.08c2060f.csv"
+  },
+  {
+    "revision": "96e2d5c086546355f8b4",
+    "url": "/intro-to-data-visualization-project/static/js/runtime~main.04e39f13.js"
+  },
+  {
+    "revision": "cfce9e92591658e0a4a7",
+    "url": "/intro-to-data-visualization-project/static/js/main.97209a11.chunk.js"
+  },
+  {
+    "revision": "bc4c42231a169fd18dd6",
+    "url": "/intro-to-data-visualization-project/static/js/2.5c492407.chunk.js"
+  },
+  {
+    "revision": "cfce9e92591658e0a4a7",
+    "url": "/intro-to-data-visualization-project/static/css/main.26072405.chunk.css"
+  },
+  {
+    "revision": "e3a9e6040cdbd990456ba493fe5e1f06",
+    "url": "/intro-to-data-visualization-project/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/intro-to-data-visualization-project/precache-manifest.8ae7a9a1716156b0ac8a9b1ca8420b2b.js"
+  "/intro-to-data-visualization-project/precache-manifest.0cd22141397477dbb24881106d02cc53.js"
 );
 
 workbox.clientsClaim();
